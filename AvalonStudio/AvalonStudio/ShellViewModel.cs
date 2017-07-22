1a98734cad7f59c5841bddb5ceb0bea91654f93a
Dan Walmsley <dan@walms.co.uk>
2017-07-22 18:31:19 +0100

    fix bad merge

--- AvalonStudio/AvalonStudio/ShellViewModel.cs
+++ AvalonStudio/AvalonStudio/ShellViewModel.cs
@@ -167,7 +167,7 @@ namespace AvalonStudio
 
             foreach (var menuItemAsReadOnlyDefinition in _menuItemGroupDefinitions)
             {
-                menuItemDefinition.Activation();
+                menuItemAsReadOnlyDefinition.Activation();
             }
 
             foreach (var extension in _menuItemDefinitions)
